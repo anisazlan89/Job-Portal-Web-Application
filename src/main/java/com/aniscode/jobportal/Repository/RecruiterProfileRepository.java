4a681fc57bb132d554f97519b6fee3be7023be41
anis <anisazlan16@gmail.com>
2025-10-24 06:37:05 +0800

    add spring security

---
+++ src/main/java/com/aniscode/jobportal/Repository/RecruiterProfileRepository.java
@@ -0,0 +1,7 @@
+package com.aniscode.jobportal.Repository;
+
+import com.aniscode.jobportal.Entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+}
--- src/main/java/com/aniscode/jobportal/Services/UsersService.java
+++ src/main/java/com/aniscode/jobportal/Services/UsersService.java
@@ -1,6 +1,10 @@
 package com.aniscode.jobportal.Services;
 
+import com.aniscode.jobportal.Entity.JobSeekerProfile;
+import com.aniscode.jobportal.Entity.RecruiterProfile;
 import com.aniscode.jobportal.Entity.Users;
+import com.aniscode.jobportal.Repository.JobSeekerProfileRepository;
+import com.aniscode.jobportal.Repository.RecruiterProfileRepository;
 import com.aniscode.jobportal.Repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,16 +16,28 @@ import java.util.Optional;
 public class UsersService {
 
     private final UsersRepository userRepository;
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final RecruiterProfileRepository recruiterProfileRepository;
 
     @Autowired
-    public UsersService(UsersRepository userRepository) {
+    public UsersService(UsersRepository userRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository) {
         this.userRepository = userRepository;
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.recruiterProfileRepository = recruiterProfileRepository;
     }
 
-    public Users addUser(Users users){
+    public void addUser(Users users){
+        //save the user entity first before creating profile
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
-        return userRepository.save(users);
+        Users savedUser = userRepository.save(users);
+        //get User Type Id
+        int userTypeId = users.getUserTypeId().getUserTypeId();
+        if (userTypeId ==1){
+            recruiterProfileRepository.save(new RecruiterProfile(savedUser));
+        }else{
+            jobSeekerProfileRepository.save(new JobSeekerProfile(savedUser));
+        }
     }
 
     public Optional<Users> getUserByEmail(String email){
