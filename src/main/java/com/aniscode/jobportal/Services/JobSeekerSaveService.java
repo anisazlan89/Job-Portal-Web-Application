84e6d9eae06fd3c7d6482aa8934e58f6e787dbae
anis <anisazlan16@gmail.com>
2025-11-05 17:24:21 +0800

    JobPostActivity, JobSeekerApply, JobSeekerSave, Service, Repository

---
+++ src/main/java/com/aniscode/jobportal/Services/JobSeekerSaveService.java
@@ -0,0 +1,27 @@
+package com.aniscode.jobportal.Services;
+
+import com.aniscode.jobportal.Entity.JobPostActivity;
+import com.aniscode.jobportal.Entity.JobSeekerProfile;
+import com.aniscode.jobportal.Entity.JobSeekerSave;
+import com.aniscode.jobportal.Repository.JobSeekerSaveRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobSeekerSaveService {
+
+    private final JobSeekerSaveRepository jobSeekerSaveRepository;
+
+    public JobSeekerSaveService(JobSeekerSaveRepository jobSeekerSaveRepository) {
+        this.jobSeekerSaveRepository = jobSeekerSaveRepository;
+    }
+
+    public List<JobSeekerSave> getCandidatesJob(JobSeekerProfile userAccountId) {
+        return jobSeekerSaveRepository.findByUserId(userAccountId);
+    }
+
+    public List<JobSeekerSave> getJobCandidates(JobPostActivity job) {
+        return jobSeekerSaveRepository.findByJob(job);
+    }
+}
